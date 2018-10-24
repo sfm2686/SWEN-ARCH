472e3d2abc42e2a730e2f0327de4c20363d300f4
Sultan Mira <sultanmira@Sultans-MacBook-Pro.local>
2018-10-23 20:08:25 -0400

    Assigned classes, work in progress to put the changes of the new homework

--- src/Battery.java
+++ src/Battery.java
@@ -1,99 +1,36 @@
-import java.net.MalformedURLException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
+import java.rmi.*;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-public class Battery implements java.rmi.Remote {
+public abstract class Battery implements Remote {
 
-	private final int PULSE_TIME = 200;
-	private final int BATTERY_SIMULATOR_TIME = 300;
-	private final double CAPACITY = 400;
-	private final double DEFAULT_STARTING_ENERGY = 400;
-
-	private double energyLeft; // how much kwh left
-	private boolean working;
-	private BatteryCheckerInterface batteryChecker; // remote batteryChecker object
-
-	public Battery(BatteryCheckerInterface batteryChecker) throws RemoteException {
+	// consts
+	protected final int PULSE_TIME = 200;
+	protected final int BATTERY_SIMULATOR_TIME = 300;
+	protected final double CAPACITY = 400;
+	protected final double DEFAULT_STARTING_ENERGY = 400;
+	
+	
+	protected double energyLeft; // how much kwh left
+	protected boolean working;
+	protected BatteryCheckerInterface batteryChecker; // remote batteryChecker object
+	
+	
+	public Battery(BatteryCheckerInterface checker) throws RemoteException {
 		this.energyLeft = DEFAULT_STARTING_ENERGY;
 		this.working = true;
-		this.batteryChecker = batteryChecker;
+		this.batteryChecker = checker;
 		this.batteryChecker.getTimeInterval(PULSE_TIME);
 		// initialize and start battery simulator and pulse sender
-		BatterySimulator bSim = new BatterySimulator();
 		BatteryPulse bPulse = new BatteryPulse();
 		bPulse.start();
-		bSim.start();
 		// call batteryChecker object to start taking the pulse
 		this.batteryChecker.startPulse();
 		// this.pulse();
 	}
-
-	private double calculateEnergyLevel() {
-		double energyLevel = (this.energyLeft / this.CAPACITY) * 100;
-		return energyLevel;
-	}
-
-	private double calculateDistanceLeft() {
-		return Math.round((this.energyLeft * 1.5) * 100.0) / 100.0;
-	}
-
-	private class BatterySimulator extends Thread {
-
-		@Override
-		public synchronized void run() {
-			Random rand = new Random();
-			double randValue;
-			double minRange = 0;
-			double maxRange = 10;
-			while (energyLeft > 0 && working) {
-				try {
-					TimeUnit.MILLISECONDS.sleep(BATTERY_SIMULATOR_TIME);
-					// Random value between min and max range vars defined above
-					randValue = minRange + (maxRange - minRange) * rand.nextDouble();
-
-					// randomly kill battery
-					working = randValue >= 0 && randValue < 0.2 ? false : true; // 2% chance
-
-					energyLeft -= randValue;
-					// Make sure energyLeft does not go under 0
-					energyLeft = (energyLeft < 0) ? 0 : energyLeft;
-					// Update battery state and print battery
-					calculateEnergyLevel();
-					calculateDistanceLeft();
-					System.out.println(Battery.this);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			if (!working)  {
-				System.out.println("Battery died!"); // random failure
-			} else {
-				System.out.println("Battery needs to be recharged"); // happy path
-			}
-			Thread.currentThread().interrupt();
-		}
-	}
-
-	// use for debugging battery sim
-	public void startSimulation() {
-		BatterySimulator sim = new BatterySimulator();
-		sim.start();
-	}
-
-	public void causeException() {
-		Random rand =  new Random();
-		int randValue = rand.nextInt(1000 - 0) + 0;
-		if (randValue == 500) { // random number to throw an exception
-			int bad = 20 / 0;  // 0.1% chance
-		}
-	}
-
+	
 	// send pulse to remote object
-	private class BatteryPulse extends Thread {
+	protected class BatteryPulse extends Thread {
 
 		@Override
 		public synchronized void run() {
@@ -110,20 +47,18 @@ public class Battery implements java.rmi.Remote {
 			}
 		}
 	}
-
-	@Override
-	public String toString() {
-		String result = "";
-		// formatting 2 decimal places percent for energyLeft
-		result = String.format("%s%.2f%s", "Energy level: ", this.calculateEnergyLevel(), "%\n");
-		result += "Distance left: " + calculateDistanceLeft() + "km\n";
-		result += "------------------------------------------------------\n\n";
-		return result;
+	
+	public abstract void causeException();
+	public abstract void deductEnergy();
+	
+	protected double calculateEnergyLevel() {
+		// notify battery2
+		double energyLevel = (this.energyLeft / this.CAPACITY) * 100;
+		return energyLevel;
 	}
 
-	public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
-		Registry registry = LocateRegistry.getRegistry();
-		BatteryCheckerInterface bChecker = (BatteryCheckerInterface) registry.lookup("BatteryChecker");
-		Battery battery = new Battery(bChecker);
+	protected double calculateDistanceLeft() {
+		// notify battery2
+		return Math.round((this.energyLeft * 1.5) * 100.0) / 100.0;
 	}
 }
