7fb5add363905b86e053e48e6ed52b1630fd8257
Sultan Mira <sultanmira@Sultans-MacBook-Pro.local>
2018-12-10 15:33:23 -0500

    Already demoed to class

--- python/flask/app/__init__.py
+++ python/flask/app/__init__.py
@@ -1,15 +1,13 @@
 from flask import Flask
+# from flask_login import LoginManager, SecureCookieSessionInterface
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app import config
-from datetime import timedelta
 
 # app
 app = Flask(__name__)
 app.config.from_object(config.Config)
-app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(seconds=4)
-session_cookie_name = app.session_cookie_name
 
 # database
 db = SQLAlchemy(app)
--- python/flask/app/routes.py
+++ python/flask/app/routes.py
@@ -1,14 +1,16 @@
 from flask import Flask, flash, redirect, render_template, session, url_for, request
-from flask_login import current_user, login_user, login_required, logout_user
+from flask_login import current_user, login_user, logout_user, fresh_login_required
 from app.models import User
 from app.forms import LoginForm, RegistrationForm
 import os
 from app import app, db
 from werkzeug.urls import url_parse
+from datetime import timedelta
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        print('LOGGED IN')
         return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
@@ -17,24 +19,22 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user)
-        session.permanent = True
+        # session.permanent = True
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('dashboard')
         return redirect(next_page)
-    return render_template('login.html', form=form, session_name=app.session_cookie_name)
+    return render_template('login.html', form=form)
 
 @app.route('/dashboard')
-@login_required
+@fresh_login_required
 def dashboard():
-    # print('\n\n\n\n')
-    # print(app.session_cookie_name)
-    return render_template('dashboard.html', session_name=app.session_cookie_name)
+    return render_template('dashboard.html')
 
 @app.route('/profile')
-@login_required
+@fresh_login_required
 def profile():
-    return render_template('profile.html', session_name=app.session_cookie_name)
+    return render_template('profile.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -48,14 +48,21 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
-    return render_template('register.html', form=form, session_name=app.session_cookie_name)
+    return render_template('register.html', form=form)
 
-@app.route('/logout')
-@login_required
+@app.route('/logout', methods=['GET', 'POST'])
+@fresh_login_required
 def logout():
+    print("\n\n\n\n")
+    print("LOGOUT CALLED")
     logout_user()
+    if current_user.is_authenticated:
+        print('BS LOGOUT')
     return redirect(url_for('login'))
 
-# @app.before_request
-# def inactivity():
-#     app.permanent_session_lifetime = timedelta(seconds=6)
+@app.before_request
+def inactivity():
+    session.permanent = True
+    # app.permanent_session_lifetime = timedelta(minutes=40)
+    app.permanent_session_lifetime = timedelta(seconds=4)
+    session.modified = True
